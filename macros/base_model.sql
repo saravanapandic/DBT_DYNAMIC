6e1185cc5d125502fe0506925338bed53429f132
saravana <csaravanakd@gmail.com>
2024-04-23 15:20:38 +0530

    saravana

--- macros/base_model.sql
+++ macros/base_model.sql
@@ -1,15 +1,17 @@
 
-{% macro base_model_userInput(sql_query, UNIQUE_KEY=[], ARRIVAL_COLUMN = None, materialized="incremental") %}
-  {{ return(adapter.dispatch('base_model_userInput', 'ownmacro')(sql_query, UNIQUE_KEY, ARRIVAL_COLUMN, materialized)) }}
+{% macro base_model_userInput(sql_query, UNIQUE_KEY=[], ARRIVAL_COLUMN = None, materialized="incremental" , sql_incr_cln ="fdf" , model_incr_cln="dfsdf") %}
+  {{ return(adapter.dispatch('base_model_userInput', 'ownmacro')(sql_query, UNIQUE_KEY, ARRIVAL_COLUMN, materialized, sql_incr_cln, model_incr_cln)) }}
 {% endmacro %}
 
-{% macro default__base_model_userInput(sql_query, UNIQUE_KEY, ARRIVAL_COLUMN, materialized) %}
+{% macro default__base_model_userInput(sql_query, UNIQUE_KEY, ARRIVAL_COLUMN, materialized,sql_incr_cln, model_incr_cln) %}
 {% set SQL_model =ctes_input(sql_query) %}
 {% set base_model_sql %}
-
 {{ "{{ config(materialized='" ~ materialized ~ "',unique_key=" ~ UNIQUE_KEY ~ "}}" }}
 
 {{SQL_model }}
+
+{{ "{% if is_incremental() %}"}}
+{{"where " ~ sql_incr_cln ~ "> (select max(" ~ model_incr_cln ~") from {{this}}" }}
 
 {% endset %}
 
